a534b46665e57c294aa616319dc3913186e1420b
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-07-26 08:12:12 +0200

    pep8 and some other minor stuff

--- usage.py
+++ usage.py
@@ -13,11 +13,11 @@ import requests
 # b) complete weeks (this month)
 
 #
-## Inputs
+# Inputs
 #
 
 year = 2017
-months_range = range(6, 8) # range(1, 13) ## for the entire year
+months_range = range(6, 8)  # range(1, 13) ## for the entire year
 
 clouds = ["exoscale-ch-gva", "exoscale-ch-dk", "open-telekom-de1"]
 users = ["meb", "test"]
@@ -25,7 +25,7 @@ users = ["meb", "test"]
 # users = ["meb"]
 
 #
-## Constants
+# Constants
 #
 
 username = os.environ['SLIPSTREAM_USERNAME']
@@ -33,9 +33,9 @@ password = os.environ['SLIPSTREAM_PASSWORD']
 
 filename_template = "metrics--{year}-{month}--{detail}.csv"
 
-url_template_base = "https://nuv.la/api/usage?$filter=frequency='{frequency}' " \
-                    "and start-timestamp>={year}-{month_start}-01 " \
-                    "and end-timestamp<={year}-{month_end}-01"
+url_template_base = "https://nuv.la/api/usage?$filter=frequency='{frequency}'" \
+                    " and start-timestamp>={year}-{month_start}-01" \
+                    " and end-timestamp<={year}-{month_end}-01"
 
 body_template = "..."
 
@@ -44,26 +44,26 @@ url_login = "https://nuv.la/auth/login"
 cookies = None
 
 #
-## Functions
+# Functions
 #
 
 
 def _build_query(users, clouds):
     users_query_fragment = " or ".join(map(lambda x: "user='%s'" % x, users))
     clouds_query_fragment = " or ".join(map(lambda x: "cloud='%s'" % x, clouds))
-    query = url_template_base + " and (%s) and (%s)" % (users_query_fragment, clouds_query_fragment)    
+    query = url_template_base + " and (%s) and (%s)" % (users_query_fragment,
+                                                        clouds_query_fragment)
     return query
 
 
 def build_query(frequency, month_start):
-    return _build_query(users,clouds).format(frequency=frequency,
-                                      month_start=month_start,
-                                      month_end=get_month_end(month_start),
-                                      year=year)
+    return _build_query(users, clouds).\
+        format(frequency=frequency, month_start=month_start,
+               month_end=get_month_end(month_start), year=year)
 
 
 def get_month_end(start):
-    return 1 if start >= 12 else start+1 
+    return 1 if start >= 12 else start+1
 
 
 def test_response_raise(response, message):
@@ -82,12 +82,13 @@ def reduce(resp, group_by, include_all):
     # For example: group_by=user, include_all=cloud
     # each entry (line at the end) correspond to a user, aggregating all clouds
     # return a set, with each entry corresponding to the group_by field
-    metrics = {} # 1 entry per group_by
+    metrics = {}  # 1 entry per group_by
     for entry in resp['usages']:
         group_by_value = entry[group_by]
         include_all_value = entry[include_all]
-        #print("grouping by %s = %s" % (group_by, group_by_value))
-        metrics[group_by_value] = metrics.get(group_by_value, {include_all: set()})
+        # print("grouping by %s = %s" % (group_by, group_by_value))
+        metrics[group_by_value] = metrics.get(group_by_value,
+                                              {include_all: set()})
         metrics[group_by_value][include_all].add(include_all_value)
         for metric_name, metrics_value in entry['usage'].iteritems():
             for k, v in metrics_value.iteritems():
@@ -113,7 +114,7 @@ def convert(metric, value):
     if 'disk' == metric:
         return value / 60  # / 1024 # return GB-hour from GB-minutes
     try:
-        return value / 60 # return per minute??!
+        return value / 60  # return per minute??!
     except(TypeError):
         return value
 
@@ -122,7 +123,8 @@ def format(clouds_metrics, group_by, include_all, filename):
     columns = ["vm", "cpu", "ram", "disk", include_all]
     with open(filename, 'w') as f:
         f.write("%s, VM (hours), CPU (hours), RAM (GB-hours), "
-                "Disk (GB-hours), Included %ss\n" % (group_by.title(), include_all.title()))
+                "Disk (GB-hours), Included %ss\n" % (group_by.title(),
+                                                     include_all.title()))
         for group in clouds_metrics:
             f.write("%s, " % group)
             for c in columns:
@@ -145,7 +147,7 @@ def process_for_month(frequency, month_start):
     url = build_query(frequency, month_start)
     response = get_metrics(url)
     # First reduce for all users, then for all clouds
-    for group_by, include_all in [["user", "cloud"], ["cloud", "user"]]: # group_by, include_all
+    for group_by, include_all in [["user", "cloud"], ["cloud", "user"]]:
         clouds_metrics = reduce(response, group_by, include_all)
         filename = filename_template.format(
             year=year,
@@ -171,26 +173,20 @@ def processing_loop():
     if use_weekly:
         # Process last month as weekly, assuming it is not complete
         m = months_range[-1]
-        print('Processing current month using weekly summary: %s...' % (calendar.month_name[m]))
+        print('Processing current month using weekly summary: %s...' %
+              calendar.month_name[m])
         frequency = 'weekly'
         process_for_month(frequency, m)
 
 
-# def merge_files():
-#     filenames = map(lambda m: 'metrics-%s.csv' % pad_filename(m), months_range)
-#     with open('metrics.csv', 'w') as o:
-#         for filename in filenames:
-#             with open(filename) as infile:
-#                 o.write('filename: %s\n' % filename)
-#                 o.write(infile.read())
-
-# Compute the following usage aggregation:
-# - for all given users, all given clouds
-# - for all given clouds, all given users
-# - for all given user and cloud
-# a) complete months (from given range)
-# b) complete weeks (this month)
+def merge_files():
+    filenames = map(lambda m: 'metrics-%s.csv' % pad_filename(m), months_range)
+    with open('metrics.csv', 'w') as o:
+        for filename in filenames:
+            with open(filename) as infile:
+                o.write('filename: %s\n' % filename)
+                o.write(infile.read())
 
 cookies = login()
 processing_loop()
-#merge_files()
+# merge_files()
